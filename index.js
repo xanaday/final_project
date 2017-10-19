1956db28f602932bc6562a7930a04f0c17af5d48
xanaday <kate.kolbell@gmail.com>
2017-10-18 21:19:02 -0400

    modal updates

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-var express = require('express');
-var app = express();
+'use strict'
+
+const express = require('express');
+const app = express();
+const parser = require('body-parser');
+
+app.use(parser.urlencoded({ extended: false })); // global use of body-parser 
+app.use(express.static('public')); // static assets are service under the 'public' folder
+app.set('view engine', 'ejs'); // ejs as the rendering engine
 
 app.get('/', function(req, res) {
-    res.send('hello world')
+    res.render('index.ejs')
 });
 
 app.listen(3000, function(){
